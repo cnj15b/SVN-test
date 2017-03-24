552ef2debcadf3f34392267a566eca62499127ae
cnj15b <cnj15b@my.fsu.edu>
2017-03-24 16:49:04 -0500

    Update svn_test.cpp

--- svn_test.cpp
+++ svn_test.cpp
@@ -4,6 +4,7 @@
 int main()
 {
 	std::cout << "Welcome to the Syncro SVN Client\n"; //comment added to test commits
-  std::cout << "Enjoy your 30 day free trial!\n";
+        std::cout << "Enjoy your 30 day free trial!\n";
+	std::cout << "Have a nice day!\n";
 	return 0;
 }
